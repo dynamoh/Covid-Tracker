f3868d99381edb1051133dc2a9b4fc91f5ffd9e1
dynamoh <guptaheet53@gmail.com>
2020-09-14 22:19:34 +0530

    served media files

--- Covid/urls.py
+++ Covid/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+
 from .views import GetPatientsInfoView, AddPatientsInfoView, sendMail
 
 from .populate_data import populate_patients
@@ -12,3 +16,9 @@ urlpatterns = [
     # Upload patients data from csv
     path('upload/patients/AXz123/data/', populate_patients, name="PopulatePatients"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+if not settings.DEBUG:
+    urlpatterns += [ path('media/<str:path>/', serve,{'document_root': settings.MEDIA_ROOT}), path('static/<str:path>/', serve,{'document_root': settings.STATIC_ROOT}), ]
